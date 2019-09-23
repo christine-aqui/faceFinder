108e391fe92618692e3ada5959b0f077c79e2fae
Christine <aqui.christine@gmail.com>
2019-09-22 20:44:22 -0400

    added registration page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Registration from './components/Registration/Registration';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -37,8 +39,9 @@ class App extends Component {
 		super();
 		this.state = {
 			input: '',
-			imageUrl: 'https://cdn.onlinewebfonts.com/svg/img_94880.png',
+			imageUrl: '',
 			box: {},
+			route: 'signin',
 		};
 	}
 
@@ -50,13 +53,13 @@ class App extends Component {
 		return {
 			leftCol: clarifaiFace.left_col * width,
 			topRow: clarifaiFace.top_row * height,
-			rightCol: width - (clarifaiFace.right_col * width),
-			bottomRow: height - (clarifaiFace.bottom_row * height),
+			rightCol: width - clarifaiFace.right_col * width,
+			bottomRow: height - clarifaiFace.bottom_row * height,
 		};
 	};
 
 	displayFaceBox = box => {
-		console.log('box obj ', box)
+		console.log('box obj ', box);
 		this.setState({ box: box });
 	};
 
@@ -67,22 +70,36 @@ class App extends Component {
 	onButtonSubmit = event => {
 		this.setState({ imageUrl: this.state.input });
 
-		app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-		.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-		.catch(err => console.log(err))
+		app.models
+			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+			.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+			.catch(err => console.log(err));
+	};
+
+	onRouteChange = route => {
+		this.setState({ route: route });
 	};
 
 	render() {
 		return (
 			<div className="App">
 				<Particles className="particles" params={particlesOptions} />
-				<Navigation />
-				{/*
-        <Logo />
-      */}
-				<Rank />
-				<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-				<FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
+				<Navigation onRouteChange={this.onRouteChange} />
+				{this.state.route === 'home' ? (
+					<div>
+						<Logo />
+						<Rank />
+						<ImageLinkForm
+							onInputChange={this.onInputChange}
+							onButtonSubmit={this.onButtonSubmit}
+						/>
+						<FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
+					</div>
+				) : this.state.route === 'signin' ? (
+					<Signin onRouteChange={this.onRouteChange} />
+				) : (
+					<Registration onRouteChange={this.onRouteChange} />
+				)}
 			</div>
 		);
 	}
