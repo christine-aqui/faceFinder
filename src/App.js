25be903628d3414c858324319f7832eec6f86dbb
Christine <aqui.christine@gmail.com>
2019-09-29 22:43:04 -0400

    checkpoint

--- src/App.js
+++ src/App.js
@@ -46,6 +46,12 @@ class App extends Component {
 		};
 	}
 
+	// componentDidMount() {
+	// 	fetch('http://localhost:3000')
+	// 	.then(res => res.json())
+	// 	.then(console.log)
+	// }
+
 	calculateFaceLocation = data => {
 		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
 		const image = document.getElementById('inputImage');
